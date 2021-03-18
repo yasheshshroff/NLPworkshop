a4d4569349d68c3e4d4ab4b5e270112f6c9d4927
yasheshshroff <yshroff@gmail.com>
2021-03-18 03:32:22 -0700

    updated

--- labs/airflow.sh
+++ labs/airflow.sh
@@ -2,4 +2,3 @@ wget https://raw.githubusercontent.com/yasheshshroff/NLPworkshop/main/labs/07a_d
 wget https://raw.githubusercontent.com/yasheshshroff/NLPworkshop/main/labs/07a_predict_disaster_tfidf.py -O predict_disaster_tfidf.py
 wget https://github.com/yasheshshroff/NLPworkshop/raw/main/labs/dataset/disaster_data.zip
 unzip disaster_data.zip
-head -10 disaster_data/train.csv > disaster_data/predict.csv
